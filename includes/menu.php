602c3fb5ac1f65e1286865ffab1a95bde37029bf
Robin Cawser <robincawser@gmail.com>
2012-09-17 20:22:05 +0100

    Preparing for new version

--- includes/menu.php
+++
@@ -1,7 +0,0 @@
-<ul id="menu">
-		<li><a href="index.php">Home</a></li>
-		<li><a href="live.php">Live</a></li>
-		<!--<li><a href="archive.php">Archive</a></li>-->
-		<li><a href="statistics.php">Stats</a></li>
-		<li><a href="http://robcaw.com/motionphp/#documentation">Help</a></li>
-</ul>
