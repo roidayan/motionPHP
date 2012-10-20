1cf62b5e90c60dd20a7f10c41d265576e69aa76c
Roi Dayan <roi.dayan@gmail.com>
2012-10-20 07:50:19 -0200

    Add html titles to icons in detail list.
    Signed-off-by: Roi Dayan <roi.dayan@gmail.com>

--- includes/detail_list.php
+++ includes/detail_list.php
@@ -9,9 +9,9 @@
 		echo '	<li><i class="icon-calendar"></i> '.date('j/n/y H:i:s',$timestamp).'</li>';	
 	}	
 	//Format the date to show the length of an event.
-	echo '	<li><i class="icon-time"></i> '.date('H:i:s', $row_details['length']).'</li>';
+	echo '	<li><i title="Length" class="icon-time"></i> '.date('H:i:s', $row_details['length']).'</li>';
 	
 	//Camera number
-	echo '	<li><i class="icon-camera"></i> '.$row_details['camera'].'</li>';
-	echo '	<li><i class="icon-film"></i> '.$row_details['frame_count'].'</li>';
+	echo '	<li><i title="Camera id" class="icon-camera"></i> '.$row_details['camera'].'</li>';
+	echo '	<li><i title="Frame count" class="icon-film"></i> '.$row_details['frame_count'].'</li>';
 
