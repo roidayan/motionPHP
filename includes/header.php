08add761a898e20f8e543073d8b950c304a11922
Roi Dayan <roi.dayan@gmail.com>
2012-10-24 23:54:14 +0200

    Fix iphone viewport
    Signed-off-by: Roi Dayan <roi.dayan@gmail.com>

--- includes/header.php
+++ includes/header.php
@@ -26,6 +26,7 @@ elseif(strstr($page,'statistics.php'))
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
 <script src="js/jquery-1.8.1.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
 <link href="css/bootstrap.css" rel="stylesheet">
@@ -35,17 +36,24 @@ elseif(strstr($page,'statistics.php'))
 </head>
 <body>
 	
-		<div class="navbar navbar-static-top ">
+		<div class="navbar navbar-fixed-top ">
 			
 			<div class="navbar-inner">
 				<div class="container">
+					<a class="btn btn-navbar collapsed" data-toggle="collapse" data-target=".nav-collapse">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</a>
 					<a class="brand" href="index.php">motionPHP</a>
-					<ul class="nav">
-						<li <?php if(strstr($page,'index.php')||strstr($page,'event.php')){echo 'class="active"';}?>><a href="index.php">Events</a></li>
-						<li <?php if(strstr($page,'live.php')){echo 'class="active"';}?>><a href="live.php">Live</a></li>
-						<li <?php if(strstr($page,'statistics.php')){echo 'class="active"';}?>><a href="statistics.php">Stats</a></li>
-						<li><a href="http://robcaw.com/motionphp/">Help</a></li>
-					</ul>
+					<div class="nav-collapse">
+						<ul class="nav">
+							<li <?php if(strstr($page,'index.php')||strstr($page,'event.php')){echo 'class="active"';}?>><a href="index.php">Events</a></li>
+							<li <?php if(strstr($page,'live.php')){echo 'class="active"';}?>><a href="live.php">Live</a></li>
+							<li <?php if(strstr($page,'statistics.php')){echo 'class="active"';}?>><a href="statistics.php">Stats</a></li>
+							<li><a href="http://robcaw.com/motionphp/">Help</a></li>
+						</ul>
+					</div>
 				</div>
 			</div>
 		</div>
